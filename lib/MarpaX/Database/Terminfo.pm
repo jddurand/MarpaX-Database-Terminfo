85c45930ff8415231a7c048104eaef6049eed2eb
Jean-Damien Durand <jeandamiendurand@free.fr>
2013-08-02 12:19:28 +0200

    Initial commit

---
+++ lib/MarpaX/Database/Terminfo.pm
@@ -0,0 +1,13 @@
+use strict;
+use warnings FATAL => 'all';
+
+package MarpaX::Database::Terminfo;
+
+# ABSTRACT: Parse a terminfo data base using Marpa
+
+use Log::Any qw/$log/;
+use Carp qw/croak/;
+
+# VERSION
+
+1;
