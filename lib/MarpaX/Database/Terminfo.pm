b9f74f04222bcbc8c2ad12d625a20f29e967b581
Jean-Damien Durand <jeandamiendurand@free.fr>
2013-08-07 11:39:43 +0200

    Doc

--- lib/MarpaX/Database/Terminfo.pm
+++ lib/MarpaX/Database/Terminfo.pm
@@ -13,19 +13,40 @@ use Carp qw/croak/;
 
 # VERSION
 
-sub new {
-  my $class = shift;
+=head1 DESCRIPTION
 
-  my $self = {};
+This module parses a terminfo database and produces an AST from it. If you want to enable logging, be aware that this module is a Log::Any thingy.
 
-  my $grammarObj = MarpaX::Database::Terminfo::Grammar->new(@_);
-  $self->{_G} = Marpa::R2::Scanless::G->new({source => \$grammarObj->content, bless_package => __PACKAGE__});
-  $self->{_R} = Marpa::R2::Scanless::R->new({grammar => $self->{_G}});
+The grammar is the one found at L<http://nixdoc.net/man-pages/HP-UX/man4/terminfo.4.html#Formal%20Grammar>.
 
-  bless($self, $class);
+=head1 SYNOPSIS
 
-  return $self;
-}
+    use strict;
+    use warnings FATAL => 'all';
+    use MarpaX::Database::Terminfo;
+    use Log::Log4perl qw/:easy/;
+    use Log::Any::Adapter;
+    use Log::Any qw/$log/;
+    #
+    # Init log
+    #
+    our $defaultLog4perlConf = '
+    log4perl.rootLogger              = WARN, Screen
+    log4perl.appender.Screen         = Log::Log4perl::Appender::Screen
+    log4perl.appender.Screen.stderr  = 0
+    log4perl.appender.Screen.layout  = PatternLayout
+    log4perl.appender.Screen.layout.ConversionPattern = %d %-5p %6P %m{chomp}%n
+    ';
+    Log::Log4perl::init(\$defaultLog4perlConf);
+    Log::Any::Adapter->set('Log4perl');
+    #
+    # Parse terminfo
+    #
+    my $terminfoSourceCode = "ansi|ansi/pc-term compatible with color,\n\tmc5i,\n";
+    my $terminfoAstObject = MarpaX::Database::Terminfo->new();
+    $terminfoAstObject->parse(\$terminfoSourceCode)->value;
+
+=cut
 
 our $I_CONSTANT = qr/(?:(0[xX][a-fA-F0-9]+(?:[uU](?:ll|LL|[lL])?|(?:ll|LL|[lL])[uU]?)?)             # Hexadecimal
                       |([1-9][0-9]*(?:[uU](?:ll|LL|[lL])?|(?:ll|LL|[lL])[uU]?)?)                    # Decimal
@@ -77,6 +98,38 @@ my %events = (
 	pos(${$bufferp}) = $prev;
     },
 );
+
+=head1 SUBROUTINES/METHODS
+
+=cut
+
+# ----------------------------------------------------------------------------------------
+=head2 new($class)
+
+Instantiate a new object. Takes no parameter.
+
+=cut
+
+sub new {
+  my $class = shift;
+
+  my $self = {};
+
+  my $grammarObj = MarpaX::Database::Terminfo::Grammar->new(@_);
+  $self->{_G} = Marpa::R2::Scanless::G->new({source => \$grammarObj->content, bless_package => __PACKAGE__});
+  $self->{_R} = Marpa::R2::Scanless::R->new({grammar => $self->{_G}});
+
+  bless($self, $class);
+
+  return $self;
+}
+# ----------------------------------------------------------------------------------------
+=head2 parse($self, $bufferp)
+
+Parses a terminfo database. Takes a pointer to a string as parameter.
+
+=cut
+
 sub parse {
     my ($self, $bufferp) = @_;
 
@@ -94,12 +147,40 @@ sub parse {
             $self->{_R}->$code($bufferp, $str, $start, $length);
         }
     }
+
+    return $self;
 }
+# ----------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------
+=head2 value($self)
+
+Returns Marpa's value on the parse tree. Ambiguous parse tree result is disabled and the module will croak if this happen.
+
+=cut
 
 sub value {
     my ($self) = @_;
 
-    return $self->{_R}->value();
+    my $rc = $self->{_R}->value();
+
+    #
+    # Another parse tree value ?
+    #
+    if (defined($self->{_R}->value())) {
+	my $msg = 'Ambigous parse tree detected';
+	if ($log->is_fatal) {
+	    $log->fatalf('%s', $msg);
+	}
+	croak $msg;
+    }
+    if (! defined($rc)) {
+	my $msg = 'Parse tree failure';
+	if ($log->is_fatal) {
+	    $log->fatalf('%s', $msg);
+	}
+	croak $msg;
+    }
+    return $rc
 }
 
 1;
